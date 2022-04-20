cbbb3799fa48cd078200789467e9f800ad8c118c
omar1a4 <103324477+omar1a4@users.noreply.github.com>
2022-04-20 17:56:48 +0300

    reverted back to original file

--- RLEList.h
+++ RLEList.h
@@ -8,11 +8,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-//test functions:
-int returnSize(RLEList list);
-
-//-------------------------------------------------------------------------------
-
 /**
 * Run Length Encoding List
 *
