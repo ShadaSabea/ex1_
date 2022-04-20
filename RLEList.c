14381b4b99008b256a50fd889dbc2f9584b24c25
omar1a4 <103324477+omar1a4@users.noreply.github.com>
2022-04-20 17:50:27 +0300

    Added node definition and list definition and create list and create node

--- RLEList.c
+++ RLEList.c
@@ -1,8 +1,58 @@
 #include "RLEList.h"
+#include<stdlib.h>
+
+typedef struct node{
+    char val;
+    int valCount;
+    struct node* next;
+    struct node* previous;
+}*Node;
 
 struct RLEList_t{
-    //TODO: implement
+    Node listHead;
+    int listLen;
 };
-
 //implement the functions here
 
+
+RLEList RLEListCreate()
+{
+    RLEList newList = malloc(sizeof(*newList));
+    if(!newList)
+    {
+        return NULL;
+    }
+    newList->listHead=NULL;
+    newList->listLen = 0;
+    return newList;
+}
+
+Node createNode(char val)
+{
+    Node newNode=malloc(sizeof(*newNode));
+    if(!newNode)
+    {
+        return NULL;
+    }
+    newNode->val=val;
+    newNode->valCount=1;
+    newNode->next=NULL;
+    newNode->previous=NULL;
+    return newNode;
+}
+
+void RLEListDestroy(RLEList list)
+{
+    Node currentNode = NULL;      
+}
+
+
+
+//------------------------------------------------------------------------------
+//test functions:
+
+int returnSize(RLEList list){
+    return list->listLen;
+}
+
+//------------------------------------------------------------------------------
