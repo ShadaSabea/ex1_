8ce9f02de48f36518188bc2945f5d54be93f5d1f
layanqaddah <103147665+layanqaddah@users.noreply.github.com>
2022-04-21 16:11:00 +0300

    Update RLEList.c

--- RLEList.c
+++ RLEList.c
@@ -1,58 +1,26 @@
 #include "RLEList.h"
 #include<stdlib.h>
 
-typedef struct node{
+
+typedef struct RLEList_t{
     char val;
     int valCount;
     struct node* next;
     struct node* previous;
-}*Node;
-
-struct RLEList_t{
-    Node listHead;
-    int listLen;
-};
-//implement the functions here
-
+}*RLEList;
 
 RLEList RLEListCreate()
 {
-    RLEList newList = malloc(sizeof(*newList));
+    RLEList newList=malloc(sizeof(*newList));
     if(!newList)
     {
         return NULL;
     }
-    newList->listHead=NULL;
-    newList->listLen = 0;
+    newList->val='/0';
+    newList->valCount=1;
+    newList->next=NULL;
+    newList->previous=NULL;
     return newList;
 }
 
-Node createNode(char val)
-{
-    Node newNode=malloc(sizeof(*newNode));
-    if(!newNode)
-    {
-        return NULL;
-    }
-    newNode->val=val;
-    newNode->valCount=1;
-    newNode->next=NULL;
-    newNode->previous=NULL;
-    return newNode;
-}
 
-void RLEListDestroy(RLEList list)
-{
-    Node currentNode = NULL;      
-}
-
-
-
-//------------------------------------------------------------------------------
-//test functions:
-
-int returnSize(RLEList list){
-    return list->listLen;
-}
-
-//------------------------------------------------------------------------------
