9203538b4af22e64aeb7be94c7fc367b612ea641
omar1a4 <103324477+omar1a4@users.noreply.github.com>
2022-04-26 02:36:21 +0300

    Added AsciiArt.h

---
+++ AsciiArt.h
@@ -0,0 +1,39 @@
+#ifndef ASCII_ART
+#define ASCII_ART
+
+#include "RLEList.h"
+#include <stdio.h>
+
+
+/**
+ * @brief 
+ *   Converts the contents in a given file to RLEList
+ * @param in_stream - A descriptor to a file containing ascii art image
+ * @return 
+ *   A new RLEList that contains the characters in in_stream
+ */
+
+RLEList asciiArtRead(FILE* in_stream);
+
+
+/**
+ * @brief 
+ * 
+ * @param list 
+ * @param out_stream 
+ * @return RLEListResult 
+ */
+RLEListResult asciiArtPrint(RLEList list, FILE* out_stream);
+
+
+/**
+ * @brief 
+ * 
+ * @param list 
+ * @param out_stream 
+ * @return RLEListResult 
+ */
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE* out_stream);
+
+
+#endif
