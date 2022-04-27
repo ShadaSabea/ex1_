378e6ffc81721918a712dca7137f0b10486140eb
layanqaddah <103147665+layanqaddah@users.noreply.github.com>
2022-04-27 20:21:12 +0300

    Update AsciiArtTool.h

--- AsciiArtTool.h
+++ AsciiArtTool.h
@@ -6,31 +6,41 @@
 
 
 /**
- * @brief Converts the contents in a given file to RLEList
- * @param in_stream - A descriptor to a file containing ascii art image
- * @return 
- *   A new RLEList that contains the characters in in_stream
+ * asciiArtRead : Converts the contents in a given file to RLEList.
+ *
+ * @param in_stream - A descriptor to a file containing ascii art image.
+ * @return
+ *   Null  if a NULL was sent as one of the parameter.
+ *   A new RLEList that contains the characters in the given file (in_stream).
  */
 
 RLEList asciiArtRead(FILE* in_stream);
 
 
 /**
- * @brief 
+ * asciiArtPrint : prints the image described in a given RLE list to the given file.
  * 
- * @param list 
- * @param out_stream 
- * @return RLEListResult 
+ * @param list  - The RLE list to be written in the given file.
+ * @param out_stream  - A descriptor to a file that the image will be written in .
+ * @return
+ *  RLE_LIST_NULL_ARGUMENT if a NULL was sent as one of the parameters.
+ * 	RLE_LIST_ERROR  if the operation failed.
+ * 	LIST_SUCCESS if the image has been printed successfully.
+ *
  */
 RLEListResult asciiArtPrint(RLEList list, FILE* out_stream);
 
 
 /**
- * @brief 
- * 
- * @param list 
- * @param out_stream 
- * @return RLEListResult 
+ * asciiArtPrintEncoded : Prints encoded image to the image described
+ * in RLE list into the given file.
+ *
+ * @param list - The RLE list that describes the image ;
+ * @param out_stream - A descriptor to a file that the encoded image will be written in .
+ * @return
+ *   RLE_LIST_NULL_ARGUMENT if a NULL was sent as one of the parameters.
+ *   RLE_LIST_ERROR  if the operation failed.
+ *   LIST_SUCCESS if the encoded image has been printed successfully.
  */
 RLEListResult asciiArtPrintEncoded(RLEList list, FILE* out_stream);
 
