ef83365db8800901eb6ae9684247eb151653b9ad
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-04-29 00:24:04 +0300

    Update AsciiArtTool.c

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -1,70 +1,80 @@
- #include "AsciiArtTool.h"
-#include<stdio.h>
-#include<stdlib.h>
-
-#define CHUNK_SIZE 256
+//
+// Created by Shada Sabea on 24/04/2022.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "AsciiArtTool.h"
+#define CHUNCK_SIZE 256
 
 RLEList asciiArtRead(FILE* in_stream)
 {
-    if(!in_stream)
+    RLEList list = RLEListCreate();
+    if(list!=NULL)
     {
         return NULL;
     }
-    RLEList list=RLEListCreate();
-    if(!list)
+    if (in_stream == NULL)
     {
         return NULL;
     }
-    char buffer[CHUNK_SIZE];
-    while(fgets(buffer,CHUNK_SIZE,in_stream) != NULL)
-    {
-        int index=0;
-        while(buffer[index])
+        char buffer[CHUNCK_SIZE];
+        while (fgets(buffer, CHUNCK_SIZE, in_stream) != NULL)
         {
-            RLEListAppend(list,buffer[index++]);
+            int index = 0;
+            while (index < CHUNCK_SIZE)
+            {
+                char c = buffer[index];
+                RLEListAppend(list, c);
+                index++;
+            }
+
         }
+        return list;
     }
-    return list;
-}
 
+    RLEListResult asciiArtPrint(RLEList list, FILE *out_stream)
+    {
+        if (list==NULL ||out_stream == NULL)
+        {
+            return RLE_LIST_NULL_ARGUMENT;
+        }
 
+        RLEListResult result;
+        int size = RLEListSize(list), index;
+        char put_char[2];
+        put_char[1]='\0';
 
-RLEListResult asciiArtPrint(RLEList list, FILE* out_stream)
-{
-    if(!list || !out_stream)
+        for (index = 0; index < size; index++)
+        {
+            put_char[0] = RLEListGet(list, index, &result);
+            if(result!=RLE_LIST_SUCCESS)
+            {
+                result=RLE_LIST_ERROR;
+                return result;
+            }
+
+            fputs(put_char, out_stream);
+        }
+        return result;
+    }
+
+    RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream)
+    {
+    if(list==NULL||out_stream==NULL)
     {
         return RLE_LIST_NULL_ARGUMENT;
     }
-    int listLen = RLEListSize(list);
-    RLEListResult result;
-    char currentChar[2];
-    currentChar[1]='\0';
-    for (int i = 0; i < listLen; i++)
-    {
-        currentChar[0] = RLEListGet(list, i ,&result);
-        if(result != RLE_LIST_SUCCESS)
+        RLEListResult result;
+        char *ptr = RLEListExportToString(list, &result);
+        if (ptr == NULL)
         {
-            result=RLE_LIST_ERROR;
-            return result;
+            return RLE_LIST_ERROR;
         }
-        fputs(currentChar,out_stream);
-    }
-    return result;
-}
 
-RLEListResult asciiArtPrintEncoded(RLEList list, FILE* out_stream)
-{
-    if (!list || !out_stream)
-    {
-        return RLE_LIST_NULL_ARGUMENT;
+        fprintf(out_stream, ptr);
+        free(ptr);
+        return RLE_LIST_SUCCESS;
+
     }
-    RLEListResult result;
-    char* out=RLEListExportToString(list,&result);
-    if(!out)
-    {
-        return RLE_LIST_ERROR;
-    }
-    fprintf(out_stream,out);
-    free(out);
-    return RLE_LIST_SUCCESS;
-}
+
+
