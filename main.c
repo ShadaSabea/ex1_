d27ba635cbe36450aa90026a53ef7529a7895bd4
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-04-29 00:28:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,16 @@
 #include "AsciiArtTool.h"
 #define LENGTH 4
 #define SPACE ' '
+#define FLAG 1
+#define SOURCE 2
+#define TARGET 3
+#define ENCODED_CHAR "-e"
+#define INVERTED_CHAR "-i"
+#define ADDRESS_SIGN '@'
 
 //void print_errors(FILE* file, char* message);
-void closeFiles(FILE* output ,FILE* input);
-char alternate(char c);
+//void closeFiles(FILE* output ,FILE* input);
+char Alternate(char value);
 
 
 int main(int argc, char** argv)
@@ -17,91 +23,75 @@ int main(int argc, char** argv)
         //print_errors(stderr, "num of inputs");
         return 0;
     }
-
-    if(argv[2]==NULL)
+    if(strcmp(argv[FLAG],ENCODED_CHAR)!=0  &&  strcmp(argv[FLAG],INVERTED_CHAR)!=0)
     {
-        //print_errors(stderr, "the input file");
         return 0;
     }
 
-    FILE *inputFile=fopen((argv[2]) ,"r");
+  //  if(argv[SOURCE]==NULL)
+   // {
+        //print_errors(stderr, "the input file");
+    //    return 0;
+   // }
+    FILE *inputFile=fopen((argv[SOURCE]) ,"r");
     if(inputFile==NULL)
     {
         //print_errors(stderr,"opening a file");
         return 0;
     }
-    FILE *outputFile=fopen((argv[3]) ,"w");
+    FILE *outputFile=fopen((argv[TARGET]) ,"w");
     if(outputFile==NULL)
     {
         fclose(inputFile);
         return 0;
     }
-
-
     RLEListResult result;
-    RLEList tempList;
+    RLEList tempList ;
     tempList=asciiArtRead(inputFile);
+    if(tempList==NULL)
+    { fclose(inputFile);
+      fclose(outputFile);
+        return 0;
+     }
 
-    if(!strcmp(argv[2], "-e"))
+    if(strcmp(argv[FLAG],ENCODED_CHAR)==0)
     {
+
         result = asciiArtPrintEncoded(tempList, outputFile);
-        
-        closeFiles(outputFile,inputFile);
+        fclose(outputFile);
+        fclose(inputFile);
         RLEListDestroy(tempList);
         return 0;
 
     }
 
-    if(!strcmp(argv[2], "-i"))
+    if(strcmp(argv[FLAG], INVERTED_CHAR)==0)
     {
-        MapFunction ptr=alternate;
-        result= RLEListMap(tempList,ptr);
+        result= RLEListMap(tempList,Alternate);
         if(result!=RLE_LIST_SUCCESS)
         {
             //print_errors(stderr, "building list ");
-            closeFiles(outputFile,inputFile);
+            fclose(outputFile);
+            fclose(inputFile);
             RLEListDestroy(tempList);
             return 0;
         }
-         result=asciiArtPrint(tempList,outputFile);
-        closeFiles(outputFile,inputFile);
+        result=asciiArtPrint(tempList,outputFile);
+        fclose(outputFile);
+        fclose(inputFile);
         RLEListDestroy(tempList);
         return 0;
     }
 }
-
-
-
-
-
-void closeFiles(FILE* output ,FILE* input)
+char Alternate(char value)
 {
-    if(output!=stdin|| output ==NULL)
-    {
-        fclose(output);
-    }
-    if(input!=stdin|| input==NULL)
-    {
-        fclose(output);
-    }
-}
-
-
-
-/*void print_errors(FILE* file, char* message)
-{
-    fprintf(file, "error in: %s", message);
-}*/
-
-char alternate(char c)
-{
-    if(c=='@')
+    if(value==ADDRESS_SIGN)
     {
         return SPACE;
     }
-    if (c==SPACE)
+    if (value==SPACE)
     {
-        return '@';
+        return ADDRESS_SIGN;
     }
-    return c;
+    return value;
 }
